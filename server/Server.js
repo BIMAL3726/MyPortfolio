52547ea453aa3ffe1c6226f2eb22be8fb73d4163
BIMAL MAITY <144580882+BIMAL3726@users.noreply.github.com>
2025-07-09 18:50:15 +0530

    Update Server.js

--- server/Server.js
+++ server/Server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = ['http://localhost:3000'];
+const allowedOrigins = ['http://localhost:3000','https://myportfolio-bsnp.onrender.com'];
 
 app.use(cors({
   origin: function (origin, callback) {
