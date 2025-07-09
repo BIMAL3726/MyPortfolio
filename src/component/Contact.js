28e6c97862a5e773e8b600090dac2fcc03d00dca
BIMAL MAITY <144580882+BIMAL3726@users.noreply.github.com>
2025-07-09 18:46:02 +0530

    Update Contact.js

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -12,7 +12,7 @@ const Contact = () => {
     e.preventDefault();
 
     try {
-        const response = await fetch('http://localhost:5000/api/contact', {
+        const response = await fetch('https://myportfolio-bsnp.onrender.com/api/contact', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
